3f8568e1a9329c74a982323fb8c0ed7fae432635
Sameer Lazar <18002284@vcconnect.co.za>
2021-10-02 15:33:47 +0200

    Home and Login Screen Added

---
+++ NsaMobileApp/NsaMobileApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace NsaMobileApp.ViewModels
+{
+    public class HomeViewModel : BaseViewModel
+    {
+        public Command HomeViewModelCommand { get; }
+
+        public HomeViewModel()
+        {
+            
+        }
+
+    }
+}
--- NsaMobileApp/NsaMobileApp/ViewModels/LoginViewModel.cs
+++ NsaMobileApp/NsaMobileApp/ViewModels/LoginViewModel.cs
@@ -10,6 +10,9 @@ namespace NsaMobileApp.ViewModels
     {
         public Command LoginCommand { get; }
 
+
+
+
         public LoginViewModel()
         {
             LoginCommand = new Command(OnLoginClicked);
@@ -18,7 +21,7 @@ namespace NsaMobileApp.ViewModels
         private async void OnLoginClicked(object obj)
         {
             // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
-            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+            await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
         }
     }
 }
