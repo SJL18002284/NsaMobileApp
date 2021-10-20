167a79f7efa8e6108194a3bd9dccac9418e00143
Sameer Lazar <18002284@vcconnect.co.za>
2021-10-20 23:38:27 +0200

    Validation

---
+++ NsaMobileApp/NsaMobileApp/Behaviors/EmailValidationBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace NsaMobileApp.Behaviors
+{
+    public class EmailValidationBehavior : Behavior<Entry>
+    {
+        
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += HandleTextChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+         void HandleTextChanged(object sender, TextChangedEventArgs e)
+        {
+            //variable that will store the new value in a entry
+            var email = e.NewTextValue;
+
+            //email-regex that be will be compared to the users entry
+            var emailRegex = @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
+
+            //user entered value that will be matched
+            var emailEntered = sender as Entry;
+
+            if (Regex.IsMatch(email, emailRegex))
+            {
+                emailEntered.BackgroundColor = Color.White;
+            }
+            else
+            {
+                emailEntered.BackgroundColor = Color.Red;
+            }
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= HandleTextChanged;
+            base.OnDetachingFrom(bindable);
+        }
+    }
+}
---
+++ NsaMobileApp/NsaMobileApp/Models/DonationRequestModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace NsaMobileApp.Models
+{
+    class DonationRequestModel
+    {
+        
+        //variables of the request
+        
+        public int requestID { get; set; }
+
+        //[Required(ErrorMessage = "Request Date cannot be empty")]
+        public string requestDate { get; set; }
+        
+        //[Required(ErrorMessage = "Cause Name cannot be empty")]
+        public string causeName { get; set; }
+        
+        //[Required(ErrorMessage = "Organizer Name cannot be empty")]
+        public string organizerName { get; set; }
+        
+        //[Required(ErrorMessage = "Organizer Email cannot be empty")]
+        public string organizerEmail { get; set; }
+        
+        //[Required(ErrorMessage = "Organizer ContactNo cannot be empty")]
+        public int organizerContactNo { get; set; }
+        
+        //[Required(ErrorMessage = "Request Details cannot be empty")]
+        public string requestDetails { get; set; }
+
+        //empty constructor
+
+        public DonationRequestModel()
+        {
+
+        }
+
+        //constructor accepting parameters
+        public DonationRequestModel(int requestID, string requestDate, string causeName, string organizerName, string organizerEmail, int organizerContactNo, string requestDetails)
+        {
+            this.requestID = requestID;
+            this.requestDate = requestDate;
+            this.causeName = causeName;
+            this.organizerName = organizerName;
+            this.organizerEmail = organizerEmail;
+            this.organizerContactNo = organizerContactNo;
+            this.requestDetails = requestDetails;
+        }
+
+        //constructor without ID
+        public DonationRequestModel(string requestDate, string causeName, string organizerName, string organizerEmail, int organizerContactNo, string requestDetails)
+        {
+            this.requestDate = requestDate;
+            this.causeName = causeName;
+            this.organizerName = organizerName;
+            this.organizerEmail = organizerEmail;
+            this.organizerContactNo = organizerContactNo;
+            this.requestDetails = requestDetails;
+        }
+
+
+    }
+}
